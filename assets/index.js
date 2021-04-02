233b1f7b2bc13595ebd96e3541af6bbd7231d9cb
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-04-02 10:39:02 -0400

    AskBCS

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,12 @@
 var cityInputEl = document.querySelector("#usercity");
 var userFormEl = document.querySelector("#userform");
 
+var titleEl = document.querySelector("#cityname");
+var temperatureEl = document.querySelector("#temperature");
+var humidityEl = document.querySelector("#humidity");
+var windSpeedEl = document.querySelector("#wind");
+var uvEl=document.querySelector("#uv");
+
 
 
 var formSubmitHandler = function(event) {
@@ -9,27 +15,61 @@ var formSubmitHandler = function(event) {
     var usercity = cityInputEl.value.trim();
 
     console.log("usercity", usercity);
-    getUserWeather(usercity);
+    getLatLon(usercity);
 };
 
-var getUserWeather = function(userCity) {
+var getLatLon = function(userCity) {
     //format the user's city api url
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity +"&appid=25c5a8418c884750202fef3ff9571c32&units=metric";
+    var apiLatLonUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity +"&appid=25c5a8418c884750202fef3ff9571c32&units=metric";
     
     //make request to url
-    fetch(apiUrl).then(function(response){
+    fetch(apiLatLonUrl).then(function(response){
         if(response.ok) {
             response.json().then(function(data) {
                 console.log("data", data);
+
+                var longtitude = data.coord.lon;
+                var lattitude = data.coord.lat;
+                console.log(longtitude);
+               
+                getUserWeather(lattitude,longtitude);
+
             });  
         }
+       
     })
 };
 
-var displayWeather = function() {
-    var titleEl = document.querySelector("#cityname");
-    titleEl.textContent = "http://api.openweathermap.org/data/2.5/weather?q=" + userCity +"&appid=25c5a8418c884750202fef3ff9571c32&units=metric";
+var displayTitle = function(city,date) {}
+
+   //make a function that passes the long and lat 
+   var getUserWeather = function(lattitude, longtitude){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" +longtitude + "&exclude=minutely,hourly,alerts&appid=25c5a8418c884750202fef3ff9571c32&units=metric";
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok) {
+            response.json().then(function(data){
+                console.log(data);
+                displayWeather(data);
+                
+            });
+        }
+        
+    });
+    
 }
+var displayWeather = function(data) {
+    console.log("displayWeather data", data);
+   
+    titleEl.textContent = data.timezone + data.current.dt + data.current.weather.icon;
+
+    temperatureEl.textContent = "Temperature: " + data.current.temp;
+    humidityEl.textContent = "Humidity: " + data.current.humidity;
+    windSpeedEl.textContent = "Wind Speed: " + data.current.wind_speed;
+    uvEl.textContent = "UV Index: " + data.current.uvi;
+};
+
+
  
 userFormEl.addEventListener("submit", formSubmitHandler);
 
